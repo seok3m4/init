58298347fd25aec8dd391db612feb6161b6a9005
백승진 <vromotion@gmail.com>
2026-07-04 18:34:57 +0900

    fix(infra): main Terraform S3 backend 초기화 보정
    - main stack에 S3 backend 선언을 추가해 backend-main.hcl이 적용되도록 수정

--- infra/aws/versions.tf
+++ infra/aws/versions.tf
@@ -1,6 +1,8 @@
 terraform {
   required_version = ">= 1.10.0, < 2.0.0"
 
+  backend "s3" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,4 +10,3 @@ terraform {
     }
   }
 }
-
